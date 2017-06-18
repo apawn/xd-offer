9171cdbe5bbbeae238d59768e96410a9819c3a8f
hufei <gonnaslience@gmail.com>
2017-06-18 20:50:26 +0800

    最终

---
+++ src/components/test/test.vue
@@ -0,0 +1,20 @@
+<script>    
+
+var router = new VueRouter();
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+export var router = new VueRouter();
+
+
+</script>
+
+<template>
+    <script src="js/jQuery.js">
+    </script>
+    <script src="js/index.js"></script>
+    <script src="js/header.js"></script>
+    <script src="js/event.js"></script>
+    <script src="js/……"></script>
+</template>
